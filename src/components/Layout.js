288a654ad294b4fd4ff57c0297713ef2a3cc6aff
Atharva K <atharva47kulkarni@gmail.com>
2024-03-27 21:46:04 +0530

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+export default function Layout({children}) {
+    return (
+        <div className="w-5/6 bg-slate-200 justify-center mx-auto mt-10 h-[400px] rounded-lg p-2">
+        {children}
+        </div>
+    )
+}  
